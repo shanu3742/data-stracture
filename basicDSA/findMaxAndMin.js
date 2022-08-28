e85dc072fa771c5024e118b80426afdad1261b5d
Kumar Shanu <shanuK@aarth.io>
2022-08-28 20:18:18 +0530

    find max and min using recursive function

--- basicDSA/findMaxAndMin.js
+++ basicDSA/findMaxAndMin.js
@@ -1,4 +1,4 @@
-let array = [20, 10, -29, 30, 100, -60, 800, -90];
+let array = [20, 10, -29, 30, 100, -60, 800, -90, 1900];
 
 const findMinAndMax = (a) => {
   let min = a[0];
@@ -13,3 +13,26 @@ const findMinAndMax = (a) => {
   return `minimum number is :${min} and maximum number is :${max}`;
 };
 console.log(findMinAndMax(array));
+
+/**|
+ * find  max and min using recursive function
+ */
+let findMinAndMaxRecursively = (a, min, max, count) => {
+  if (count >= a.length - 1) {
+    return `minimum number is :${min} and maximum number is :${max}`;
+  }
+  if (count < a.length - 1) {
+    count = count + 1;
+    if (count > 0) {
+      if (max < a[count]) {
+        max = a[count];
+        return findMinAndMaxRecursively(a, min, max, count);
+      } else {
+        min = a[count];
+        return findMinAndMaxRecursively(a, min, max, count);
+      }
+    }
+  }
+  return max;
+};
+console.log(findMinAndMaxRecursively(array, array[0], array[0], 0));
